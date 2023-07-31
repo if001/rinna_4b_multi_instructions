7272b09cc1fd24aacecd154ae0a5050cdd0bc5b9
windows_on_wsl <otomijuf.004@gmail.com>
2023-07-31 23:13:15 +0900

    fix

--- fine_tuning_sft.py
+++ fine_tuning_sft.py
@@ -8,7 +8,7 @@ from datasets import load_dataset, Dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import BitsAndBytesConfig
 # from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training, TaskType
-from trl import SFTTrainer
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 
 from utils.prompter import Prompter
 
@@ -173,12 +173,8 @@ def train(
     train_val = data["train"].train_test_split(
         test_size=val_set_size, shuffle=True, seed=42
     )
-    train_data = (
-        train_val["train"].shuffle().map(generate_and_tokenize_prompt)
-    )
-    val_data = (
-        train_val["test"].shuffle().map(generate_and_tokenize_prompt)
-    )
+    train_data = train_val["train"].shuffle()    
+    val_data = train_val["test"].shuffle()    
     print("train_data len", len(train_data))
     print("val_data", len(val_data))
 
@@ -193,32 +189,16 @@ def train(
     val_data = Dataset.from_list(val_data)
     ## --- data set ---
 
-    gradient_accumulation_steps = batch_size // micro_batch_size    
+    response_template = " ### 応答:"
+    collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
+
+    # gradient_accumulation_steps = batch_size // micro_batch_size    
     trainer = SFTTrainer(
         model,
         train_dataset=train_data,
         eval_dataset=val_data,
-        args=transformers.TrainingArguments(
-            per_device_train_batch_size=micro_batch_size,
-            gradient_accumulation_steps=gradient_accumulation_steps,
-            warmup_steps=100,
-            num_train_epochs=num_epochs,
-            learning_rate=learning_rate,
-            fp16=True,
-            logging_steps=10,
-            optim="adamw_torch",
-            evaluation_strategy="steps" if val_set_size > 0 else "no",
-            save_strategy="steps",
-            eval_steps=200 if val_set_size > 0 else None,
-            save_steps=200,
-            output_dir=output_dir,
-            save_total_limit=3,
-            load_best_model_at_end=True if val_set_size > 0 else False,
-        ),
-        data_collator=transformers.DataCollatorForSeq2Seq(
-            tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
-        ),
-        packing=True
+        formatting_func=generate_and_tokenize_prompt,
+        data_collator=collator,
     )
     trainer.train()
 
@@ -230,11 +210,9 @@ def train(
     #         self, old_state_dict()
     #     )
     # ).__get__(model, type(model))
-
-    if torch.__version__ >= "2" and sys.platform != "win32":
-        model = torch.compile(model)
-
-    trainer.train()
+    # if torch.__version__ >= "2" and sys.platform != "win32":
+    #     model = torch.compile(model)
+    # trainer.train()
 
     model.save_pretrained(output_dir)
 
