08547fef310c5ad039b6eece1761cedd3b217ea9
windows_on_wsl <otomijuf.004@gmail.com>
2023-07-31 19:58:04 +0900

    fix

--- fine_tune.py
+++ fine_tune.py
@@ -87,7 +87,7 @@ def train(
 
 
     ## --- data set ---
-    prompter = Prompter(prompt_template_name)
+    prompter = Prompter(prompt_template_name, verbose=True)
     def tokenize(prompt, add_eos_token=True):
         # there's probably a way to do this with the tokenizer settings
         # but again, gotta move fast
